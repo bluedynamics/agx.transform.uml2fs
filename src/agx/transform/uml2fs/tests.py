9ee5e8adbc2da1b1fd2b5133467b17b9a0e44183
Robert Niederreiter <office@squarewave.at>
2013-01-10 16:07:06 +0100

    tests

--- src/agx/transform/uml2fs/tests.py
+++ src/agx/transform/uml2fs/tests.py
@@ -1,28 +1,27 @@
 # Copyright BlueDynamics Alliance - http://bluedynamics.com
 # GNU General Public License Version 2
-
 import unittest
 import doctest
 import zope.component
 from pprint import pprint
 from interlude import interact
 from zope.configuration.xmlconfig import XMLConfig
-
 import agx.transform.uml2fs
+
 
 optionflags = doctest.NORMALIZE_WHITESPACE | \
               doctest.ELLIPSIS | \
               doctest.REPORT_ONLY_FIRST_FAILURE
 
+
 TESTFILES = [
-    '../transform.txt',
+    'transform.txt',
 ]
 
 
 def test_suite():
     XMLConfig('meta.zcml', zope.component)()
     XMLConfig('configure.zcml', agx.transform.uml2fs)()
-
     return unittest.TestSuite([
         doctest.DocFileSuite(
             file,
@@ -31,6 +30,7 @@ def test_suite():
                    'pprint': pprint},
         ) for file in TESTFILES
     ])
+
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
